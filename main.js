db3573954fc9fc7bc6ad0b855e59cb216c62483c
pavan <76779570+PavanVishwa@users.noreply.github.com>
2021-07-26 17:30:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX=0;
+noseY=0;
+marioX=325;
+marioY=325;
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('Canvas');
+	instializeInSetup(mario);
+    video=createCapture(VIDEO);
+	video.size(600,300);
+	video.parent('game_console');
+	
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+	console.log("Model is on");
+}
+
+function gotPoses(results){
+	if(results.length > 0){
+     noseX=results[0].pose.nose.x;
+	 noseY=results[0].pose.nose.y;
+	 console.log("nosex= "+noseX+"nosey= "+noseY);
+	}
+}
+
+
+function draw() {
+	
+game();
+}
+
+
+
+
+
+
